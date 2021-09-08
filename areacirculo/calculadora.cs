7d9169efed90106e071598fd3917b64f49666caf
edumartimi <eduardomartimiano@hotmail.com>
2021-09-08 20:27:41 -0300

    Adicionar arquivos de projeto.

---
+++ areacirculo/calculadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace areacirculo
+{
+    class calculadora
+    {
+        public double Pi= 3.14;
+        public double Circunferencia(double r)
+        { 
+            return 2.0 * Pi * r;
+        }
+        public double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+
+    }
+}
